1f4c180e445a351918dc809b7e7d583825e81fad
cindyhsu <cindyhsu@hitrust.com.tw>
2023-03-02 09:45:20 +0800

    fixed bug

--- src/main/java/com/Junit/MagnetoEffect.java
+++ src/main/java/com/Junit/MagnetoEffect.java
@@ -25,32 +25,19 @@ public class MagnetoEffect {
 		
 	}
 
-	private Point findNearestAnchorFor(Point point) {
-		simpleEntry(anchor, point);
-		 List<Point> points = anchors.stream()
-				 .map(simpleEntry(anchor, point)));
-		
-		
-		
-		return anchors.stream()
-				.map(anchor -> simpleEntry(anchor, point)))
-				.filter(entry -> entry.getValue() < Math,pow(5, 2))
-				.min(java.util.Map.Entry.comparingByValue())
-				.map(AbstractMap.SimoleEntry::getKey)
-				.orElse(null);
-		
-//		double nearestDistance = Double.MAX_VALUE;
-//		Point nearestAnchor = null;
-//		for(Point anchor: anchors) {
-//			double distance =  Math.pow(anchor.getX() - point.getX(), 2) + Math.pow(anchor.getY() - point.getY(), 2);
-//			if(distance <= Math.pow(5, 2)) {
-//				if(distance < nearestDistance) {
-//					nearestAnchor = anchor;
-//					nearestDistance = distance;
-//				}
-//			}
-//		}
-//		return nearestAnchor;
+	private Point findNearestAnchorFor(Point point) {		
+		double nearestDistance = Double.MAX_VALUE;
+		Point nearestAnchor = null;
+		for(Point anchor: anchors) {
+			double distance =  Math.pow(anchor.getX() - point.getX(), 2) + Math.pow(anchor.getY() - point.getY(), 2);
+			if(distance <= Math.pow(5, 2)) {
+				if(distance < nearestDistance) {
+					nearestAnchor = anchor;
+					nearestDistance = distance;
+				}
+			}
+		}
+		return nearestAnchor;
 	}
 
 	private SimpleEntry<Point, Double> simpleEntry(Point anchor, Point point) {
